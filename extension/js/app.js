c3ffae3b76b39e84e39b4deefd944fdcd120084d
aprilorange <aprilorange.net@icloud.com>
2015-08-12 01:45:19 +0800

    sticky header

--- extension/js/app.js
+++ extension/js/app.js
@@ -1,22 +1,84 @@
 (function(W, D){
-  var div, h1, h2, h3, p, span, textarea, button, dom, App;
+  var div, h1, h2, h3, p, a, li, span, textarea, button, dom, sticky, App;
   div = React.createFactory('div');
   h1 = React.createFactory('h1');
   h2 = React.createFactory('h2');
   h3 = React.createFactory('h3');
   p = React.createFactory('p');
+  a = React.createFactory('a');
+  li = React.createFactory('li');
   span = React.createFactory('span');
   textarea = React.createFactory('textarea');
   button = React.createFactory('button');
   dom = React.findDOMNode;
+  sticky = function(){
+    var self, header, hasScrolled;
+    $('.wrapper').addClass('sticky-enabled');
+    self = this;
+    header = $('.header.header-logged-in');
+    self.lastScrollTop = 0;
+    self.delta = 5;
+    self.navbarHeight = header.outerHeight();
+    self.didScroll = false;
+    $(W).scroll(function(event){
+      self.didScroll = true;
+      return setInterval(function(){
+        if (self.didScroll) {
+          hasScrolled();
+          return self.didScroll = false;
+        }
+      }, 250);
+    });
+    return hasScrolled = function(){
+      var st;
+      st = $(W).scrollTop();
+      if (Math.abs(self.lastScrollTop - st) <= self.delta) {
+        return;
+      }
+      if (st > self.lastScrollTop && st > self.navbarHeight) {
+        header.removeClass('nav-down').addClass('nav-up');
+      } else if (st + $(W).height() < $(D).height()) {
+        header.removeClass('nav-up').addClass('nav-down');
+      }
+      return self.lastScrollTop = st;
+    };
+  };
+  sticky();
   App = (function(){
     App.displayName = 'App';
     var prototype = App.prototype, constructor = App;
     App.init = function(){
-      var TweetBox;
+      var IdeaNav, Pen, TweetBox;
       $('.github-jobs-promotion').remove();
       $('#jobs_info_tip').remove();
       $('.dashboard-sidebar').prepend('<div class="tweetbox"></div>');
+      $('#user-links').prepend('<li class="header-nav-item ideahub-pen"></li>');
+      $('.header-nav.left').append('<li class="header-nav-item ideahub-idea-nav"></li>');
+      IdeaNav = React.createClass({
+        render: function(){
+          return a({
+            className: 'header-nav-link',
+            href: '#'
+          }, 'Ideas');
+        }
+      });
+      Pen = React.createClass({
+        handleClick: function(){
+          if ($('.tweetboxContent')) {
+            return $('.tweetboxContent').focus();
+          }
+        },
+        render: function(){
+          return a({
+            href: '#',
+            className: 'header-nav-link tooltipped tooltipped-s',
+            onClick: this.handleClick,
+            'aria-label': 'Compose some idea'
+          }, span({
+            className: 'octicon octicon-pencil'
+          }));
+        }
+      });
       TweetBox = React.createClass({
         getInitialState: function(){
           return {
@@ -37,7 +99,7 @@
           }, h3(null, 'Any dev ideas?'), div({
             className: 'boxed-group-inner tweetbox-main'
           }, textarea({
-            className: 'input-contrast',
+            className: 'input-contrast tweetboxContent',
             onChange: this.handleChange,
             ref: 'tweetboxContent'
           }), div({
@@ -48,6 +110,8 @@
           }, 'Send'))));
         }
       });
+      React.render(React.createElement(Pen), D.querySelector('.ideahub-pen'));
+      React.render(React.createElement(IdeaNav), D.querySelector('.ideahub-idea-nav'));
       return React.render(React.createElement(TweetBox), D.querySelector('.tweetbox'));
     };
     function App(){}
--- src/ls/app.ls
+++ src/ls/app.ls
@@ -6,17 +6,78 @@
   h2 = React.create-factory \h2
   h3 = React.create-factory \h3
   p = React.create-factory \p
+  a = React.create-factory \a
+  li = React.create-factory \li
   span = React.create-factory \span
   textarea = React.create-factory \textarea
   button = React.create-factory \button
 
   dom = React.findDOMNode
 
+  sticky = ->
+    $('.wrapper').add-class 'sticky-enabled'
+    self = @
+    header = $('.header.header-logged-in')
+    self.lastScrollTop = 0
+    self.delta = 5
+    self.navbarHeight = header.outerHeight!
+    self.didScroll = false
+    $(W).scroll (event) -> 
+      self.didScroll = true
+      setInterval ->
+        if self.didScroll
+          hasScrolled!
+          self.didScroll = false
+      , 250
+
+    hasScrolled = ->
+      st = $(W).scrollTop!
+      # Make sure they scroll more than delta
+      return if Math.abs(self.lastScrollTop - st) <= self.delta
+
+      # If they scrolled down and are past the navbar, add class .nav-up.
+      # This is necessary so you never see what is "behind" the navbar.
+      if st > self.lastScrollTop and st > self.navbarHeight
+        # Scroll Down
+        header.remove-class('nav-down').add-class('nav-up')
+      else if st + $(W).height! < $(D).height!
+        # Scroll Up
+        header.remove-class('nav-up').add-class('nav-down') 
+
+      self.lastScrollTop = st
+    
+
+  sticky!
+
   class App
     @init = ->
+      
       $('.github-jobs-promotion').remove!
       $('#jobs_info_tip').remove!
       $('.dashboard-sidebar').prepend '<div class="tweetbox"></div>'
+      $('#user-links').prepend '<li class="header-nav-item ideahub-pen"></li>'
+      $('.header-nav.left').append '<li class="header-nav-item ideahub-idea-nav"></li>'
+
+      IdeaNav = React.create-class do
+        render: ->
+          a do
+            class-name: 'header-nav-link'
+            href: '#'
+            \Ideas
+
+      Pen = React.create-class do
+        handleClick: ->
+          if $('.tweetboxContent') then $('.tweetboxContent').focus()
+        render: ->
+          a do
+            href: '#'
+            class-name: 'header-nav-link tooltipped tooltipped-s'
+            onClick: @handleClick
+            'aria-label': 'Compose some idea'
+            span do
+              class-name: 'octicon octicon-pencil'
+
+
       TweetBox = React.create-class do
         getInitialState: ->
           do
@@ -33,7 +94,7 @@
             div do
               class-name: 'boxed-group-inner tweetbox-main'
               textarea do
-                class-name: 'input-contrast'
+                class-name: 'input-contrast tweetboxContent'
                 onChange: @handle-change
                 ref: 'tweetboxContent'
               div do
@@ -44,7 +105,10 @@
                   \Send
 
 
+      React.render React.create-element(Pen), D.querySelector('.ideahub-pen')
+      React.render React.create-element(IdeaNav), D.querySelector('.ideahub-idea-nav')
       React.render React.create-element(TweetBox), D.querySelector('.tweetbox')
+      # end init
 
 
 
